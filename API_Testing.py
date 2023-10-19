823867f807ee3b0555edb1946e78602539fdcc67
Fawwaz <79627805+FawwazGoal@users.noreply.github.com>
2023-10-19 11:04:52 +0000

    Update API_Testing.py

--- API_Testing.py
+++ API_Testing.py
@@ -2,13 +2,9 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-# Update this URI with your actual PostgreSQL credentials
 DATABASE_URI = 'postgresql+pg8000://postgres:Fawwaz410133@localhost/etle_violations'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Silence the deprecation warning
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Violation(db.Model):
@@ -20,10 +16,9 @@ class Violation(db.Model):
     plate = db.Column(db.String(50))
     machine_number = db.Column(db.String(50))
     skeleton_number = db.Column(db.String(50))
-    
+
     def to_dict(self):
         data = {c.name: getattr(self, c.name) for c in self.__table__.columns}
-        # Remove fields that should not be in the response
         for field in ['plate', 'machine_number', 'skeleton_number']:
             data.pop(field, None)
         return data
@@ -37,14 +32,16 @@ def get_violations():
     if not all([plate, machine_number, skeleton_number]):
         return jsonify({'error': 'Missing required parameters'}), 400
 
-    violations_query = Violation.query.filter_by(
+    violation = Violation.query.filter_by(
         plate=plate,
         machine_number=machine_number,
         skeleton_number=skeleton_number
-    ).all()
+    ).first()
 
-    all_violations = [violation.to_dict() for violation in violations_query]
-    return jsonify(all_violations)
+    if violation:
+        return jsonify(violation.to_dict())
+    else:
+        return jsonify({'error': 'No matching violation found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
