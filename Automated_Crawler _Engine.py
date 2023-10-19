09a5cc5378edf32e3ccf97d2b14777cbcbd62523
Fawwaz <79627805+FawwazGoal@users.noreply.github.com>
2023-10-19 11:04:29 +0000

    Update Automated_Crawler _Engine.py

--- Automated_Crawler _Engine.py
+++ Automated_Crawler _Engine.py
@@ -13,37 +13,54 @@ def fetch_etle_data(api_endpoint, payload):
     else:
         return None
 
-# Function to insert data into PostgreSQL database
-def insert_into_db(connection, data, payload):
+# Function to check for duplicate entries
+def check_duplicate(connection, data, payload):
     cursor = connection.cursor()
-    insert_query = sql.SQL(
-        "INSERT INTO violations (location, penalty_type_id, penalty_type_en, status, capture_date, plate, machine_number, skeleton_number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    check_query = sql.SQL(
+        "SELECT * FROM violations WHERE location = %s AND penalty_type_id = %s AND penalty_type_en = %s AND status = %s AND plate = %s AND machine_number = %s AND skeleton_number = %s"
     )
-    capture_date_dt = datetime.fromtimestamp(data["captureDate"] / 1000.0)
-    cursor.execute(insert_query, (
+    cursor.execute(check_query, (
         data["location"],
         data["penaltyTypeId"],
         data["penaltyTypeEn"],
         data["status"],
-        capture_date_dt,
         payload["plate"],
         payload["machine-number"],
         payload["skeleton-number"],
     ))
-    connection.commit()
+    return cursor.fetchone()
+
+# Function to insert data into PostgreSQL database
+def insert_into_db(connection, data, payload):
+    cursor = connection.cursor()
+    if not check_duplicate(connection, data, payload):  # Insert only if duplicate doesn't exist
+        insert_query = sql.SQL(
+            "INSERT INTO violations (location, penalty_type_id, penalty_type_en, status, capture_date, plate, machine_number, skeleton_number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        )
+        capture_date_dt = datetime.fromtimestamp(data["captureDate"] / 1000.0)
+        cursor.execute(insert_query, (
+            data["location"],
+            data["penaltyTypeId"],
+            data["penaltyTypeEn"],
+            data["status"],
+            capture_date_dt,
+            payload["plate"],
+            payload["machine-number"],
+            payload["skeleton-number"],
+        ))
+        connection.commit()
 
 while True:
-    # Define API endpoint and payload
+    plate = input("Enter the plate number: ")
+    machine_number = input("Enter the machine number: ")
+    skeleton_number = input("Enter the skeleton number: ")
+    payload = {"plate": plate, "machine-number": machine_number, "skeleton-number": skeleton_number}
+
     api_endpoint = "https://belik.etle-korlantas.info/p/violation-vehicles/check-data"
-    payload = {"plate": "A1492RH", "machine-number": "4A91GD9541", "skeleton-number": "MK2NCWHANJJ018193"}
-
-    # Fetch data from ETLE system
     etle_data = fetch_etle_data(api_endpoint, payload)
-    if etle_data:
-        # Assume the first entry in the "data" list is the one you want
-        violation_data = etle_data["data"][0]
 
-        # Connect to PostgreSQL database
+    if etle_data:
+        violation_data = etle_data["data"][0]
         connection = psycopg2.connect(
             host="localhost",
             port="5432",
@@ -51,14 +68,9 @@ while True:
             user="postgres",
             password="Fawwaz410133"
         )
-
-        # Insert data into database
         insert_into_db(connection, violation_data, payload)
-
-        # Close the database connection
         connection.close()
     else:
         print("Failed to fetch data from ETLE system.")
     
-    # Sleep for 12 hours before running again
     time.sleep(12 * 60 * 60)
